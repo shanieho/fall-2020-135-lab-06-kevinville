3e8758c0b007cf58432e74268d34190b7b51ce43
kevinville <kevinlhardyal@gmail.com>
2020-10-05 19:27:03 -0400

    fixing mistake

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "funcs.h"
 #include "caesar.h"
-#include "viginere.cpp"
 int main()
 {
   std::string input;  // we need a string to take the persons input, then a character in the program to convert it to ascii.
@@ -9,11 +8,10 @@ int main()
   // Testing Lab 6A
   input = "Doggo";
   std::cout <<"Runs Lab6A with word Doggo.\n";
-  std::string plaintext = encryptVigenere("Hello World", "jojo");
-  std::cout << plaintext << "\nRuns with words Good Boy!\n";
+  test_ascii(input);
+  std::cout << "\n" << "Runs with words Good Boy!\n";
   input = "Good Boy!";
   test_ascii(input);
   std::cout << "\n-----------------\n";
   return 0;
 }
-
